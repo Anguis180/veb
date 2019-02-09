de2d1df89c412e9b0b7ff3275cdafd78c7c879c7
Anastasiya <nastyusha_parshukova@mail.ru>
2019-02-09 12:09:15 +0700

    refactoring

--- src/com/par/lec/main.kt
+++ src/com/par/lec/main.kt
@@ -1,16 +1,12 @@
 package com.par.lec
 
 fun main(args: Array<String>) {
-     if (args.isEmpty()) {
-          println( " no arguments")
-     }else{
-          for (r in args) {
-           println(r)
-          }
+    if (args.isEmpty()) {
+        println(" no arguments")
+    } else {
+        args.forEach {
+            println(it)
+        }
 
-     }
-     println("hello world")
-
-
-
+    }
 }
