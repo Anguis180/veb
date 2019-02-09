78febf64d10e2d222e5daeaf2b36eab1ab3da78d
Anastasiya <nastyusha_parshukova@mail.ru>
2019-02-09 11:59:27 +0700

    print arguments

---
+++ src/com/par/lec/main.kt
@@ -0,0 +1,16 @@
+package com.par.lec
+
+fun main(args: Array<String>) {
+     if (args.isEmpty()) {
+          println( " no arguments")
+     }else{
+          for (r in args) {
+           println(r)
+          }
+
+     }
+     println("hello world")
+
+
+
+}
