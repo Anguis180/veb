79518b18af197a7a2fdadc55dc01ffff8bb40051
Anastasiya <nastyusha_parshukova@mail.ru>
2019-02-16 11:22:26 +0700

    сделали счетчик тестов

--- test.sh
+++ test.sh
@@ -1,27 +1,65 @@
 TOTAL=0
-SXCESS=0
+SUCCESS=0
 
 echo
 echo "no params"
 ./start.sh
 ACTUAL=$?
-echo ecpected 0 actual $ACTUAL
-
+let "TOTAL++"
+if [ $ACTUAL -eq 0 ]; then
+  echo  SUCCESS. expeted 0 actual $ACTUAL
+   let "SUCCESS = $SUCCESS + 1"
+else
+  echo Fail. expeted 0 actual $ACTUAL
+fi
 
 echo
 echo "with params"
 ./start.sh param
 ACTUAL=$?
-echo ecpected 1 actual $ACTUAL
+let "TOTAL++"
+if [ $ACTUAL -eq 1 ]; then
+  echo  SUCCESS. expeted 1 actual $ACTUAL
+  let "SUCCESS = $SUCCESS + 1"
+else
+  echo Fail. expeted 0 actual $ACTUAL
+fi
+
 
 echo
 echo "with param 2"
 ./start.sh param pam
 ACTUAL=$?
-echo ecpected 2 actual $ACTUAL
+let "TOTAL++"
+if [ $ACTUAL -eq 2 ]; then
+  echo  SUCCESS. expeted 2 actual $ACTUAL
+   let "SUCCESS = $SUCCESS + 1"
+else
+  echo Fail. expeted 0 actual $ACTUAL
+fi
+
 
 echo
 echo "with param 3"
 ./start.sh param pam pam
 ACTUAL=$?
-echo ecpected 3 actual $ACTUAL
+let "TOTAL++"
+if [ $ACTUAL -eq 3 ]; then
+  echo  SUCCESS. expeted 3 actual $ACTUAL
+   let "SUCCESS = $SUCCESS + 1"
+else
+  echo Fail. expeted 0 actual $ACTUAL
+fi
+
+echo
+echo Results:
+echo $SUCCESS success tes ts out of $TOTAL
+
+echo
+if [ $TOTAL -eq $SUCCESS ]; then
+ echo all tests work
+ exit 0
+ else
+ echo not work
+ exit 1
+ fi
